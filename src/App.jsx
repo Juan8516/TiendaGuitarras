742b58e7f3c1d2a5986611d39f121821d7f59b8e
Juan Manuel Salazar <134428143+Juan8516@users.noreply.github.com>
2024-05-23 10:48:28 -0500

    Add:Productos persistentes al recargar navegador

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react"
 import Header from "./Components/Header"
 import Guitar from "./Components/Guitar"
-import {db} from "./data/db"
+import { db } from "./data/db"
 
 function App() {
 
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
   //State
-  const [data, SetData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
 
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
